db3ca3686994b18fff9d2b815c16c40adae3fbaf
Ahmed <ahmed.sattar@live.com>
2023-07-06 20:18:29 +0600

    working on stage 6

--- PySmartCalculator/main.py
+++ PySmartCalculator/main.py
@@ -1,3 +1,15 @@
+class InvalidExpressionError(Exception):
+    pass
+
+
+class CalculatorError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
 def any_digit(string: str) -> bool:
     return any(map(str.isdigit, string))
 
@@ -11,11 +23,39 @@ def valid_operator(string: str) -> bool:
 
 
 def valid_operand(data: str | int) -> bool:
-    return True if type(data) is int else data.lstrip("-").isdigit()
+    try:
+        int(data)
+    except ValueError:
+        return False
+    return True
 
 
 def valid_expression(left, op, right) -> bool:
     return all((valid_operand(left), valid_operator(op), valid_operand(right)))
+
+
+class Calculator:
+    def __init__(self):
+        self.assignments = dict()
+
+    def valid_operand(self, data: str | int) -> bool:
+        try:
+            int(data)
+        except ValueError:
+            return False
+        return True
+
+    def process_operand(self, x):
+        try:
+            return int(x)
+        except ValueError:
+            if not x.lstrip("-").isalpha():
+                raise CalculatorError
+            else:
+                if x.startswith("-"):
+                    return -self.assignments[x]
+                else:
+                    return self.assignments[x]
 
 
 def stage1():
@@ -24,78 +64,165 @@ def stage1():
 
 
 def stage2():
-    while (usr_input := input().strip()) != "/exit":
+    while True:
+        usr_input = input().strip()
+
+        if usr_input == "/exit":
+            print("Bye!")
+            exit()
+
         if usr_input:
             num_list = map(int, usr_input.split())
             print(sum(num_list))
-    print("Bye!")
 
 
 def stage3():
-    while (usr_input := input().strip()) != "/exit":
-        if usr_input == "/help":
+    while True:
+        usr_input = input().strip()
+
+        if usr_input == "/exit":
+            print("Bye!")
+            exit()
+
+        elif usr_input == "/help":
             print("The program calculates the sum of numbers")
+
         elif usr_input:
             num_list = map(int, usr_input.split())
             print(sum(num_list))
-    print("Bye!")
 
 
 def stage4():
-    while (usr_input := input().strip()) != "/exit":
-        if usr_input == "/help":
+    while True:
+        usr_input = input().strip()
+
+        if usr_input == "/exit":
+            print("Bye!")
+            exit()
+
+        elif usr_input == "/help":
             print("The program calculates the sum of numbers")
+
         elif usr_input:
             expression = usr_input.split()
+
             while len(expression) > 2:
                 operand_left = int(expression.pop(0))
-                if simplify_operators(expression.pop(0)) == "-":
+
+                if simplify_operator(expression.pop(0)) == "-":
                     operand_right = -int(expression.pop(0))
+
                 else:
                     operand_right = int(expression.pop(0))
+
                 expression.insert(0, sum((operand_left, operand_right)))
-            print(expression.pop())
-    print("Bye!")
+
+            print(int(expression.pop()))
 
 
 def stage5():
-    while (usr_input := input().strip()) != "/exit":
-        if usr_input.startswith("/"):
-            if usr_input == "/help":
-                print("The program calculates the sum of numbers")
-            else:
-                print("Unknown command")
-        elif usr_input:
-            expression = usr_input.split()
-            # print(int(expression.pop(0)))
+    while True:
+        usr_input = input().strip()
 
-            print(expression[:3])
-            # if len(expression) == 1:
-                # if not valid_operand(expression[0]):
-                    # print("Invalid expression")
-            if len(expression) == 2:
-                print("Invalid expression")
-            while len(expression) > 2:
-                # operand_left = expression.pop(0)
-                # operator = expression.pop(0)
-                # operand_right = expression.pop(0)
-                # print(f"{operand_left} {operator} {operand_right}")
-                if not valid_expression(*expression[:3]):
-                    print("Invalid expression")
-                    break
-                operand_left = int(expression.pop(0))
-                operator = simplify_operator(expression.pop(0))
-                operand_right = int(expression.pop(0))
-                if operator == "-":
-                    operand_right = -operand_right
-                expression.insert(0, sum((operand_left, operand_right)))
-            print(expression.pop())
-    print("Bye!")
+        try:
+            if usr_input == "/exit":
+                print("Bye!")
+                exit()
+
+            elif usr_input == "/help":
+                print("The program calculates the sum of numbers")
+
+            elif usr_input.startswith("/"):
+                print("Unknown command")
+
+            elif usr_input:
+                expression = usr_input.split()
+
+                if len(expression) == 1:
+                    if not valid_operand(expression[0]):
+                        raise InvalidExpressionError
+
+                if len(expression) == 2:
+                    expression.insert(0, "0")
+
+                while len(expression) > 2:
+                    if not valid_expression(*expression[:3]):
+                        raise InvalidExpressionError
+
+                    operand_left = int(expression.pop(0))
+                    operator = simplify_operator(expression.pop(0))
+                    operand_right = int(expression.pop(0))
+
+                    if operator == "-":
+                        operand_right = -operand_right
+                    expression.insert(0, sum((operand_left, operand_right)))
+
+                print(int(expression.pop()))
+
+        except InvalidExpressionError:
+            print("Invalid expression")
+
+
+def stage6():
+    while True:
+        usr_input = input().strip()
+
+        try:
+            if usr_input == "/exit":
+                print("Bye!")
+                exit()
+
+            elif usr_input == "/help":
+                print("The program calculates the sum of numbers")
+
+            elif usr_input.startswith("/"):
+                print("Unknown command")
+
+            elif usr_input:
+                if "=" in usr_input:
+                    # def assignment
+                    assignments = {}
+                    print("assign")
+                    key, value = usr_input.replace(" ", "").split("=")
+                    if not key.isalpha():
+                        print("Invalid identifier")
+                        continue
+
+                    assignments[key] = value
+
+                    print(assignments)
+
+                    continue
+
+                expression = usr_input.split()
+
+                if len(expression) == 1:
+                    if not valid_operand(expression[0]):
+                        raise InvalidExpressionError
+
+                if len(expression) == 2:
+                    expression.insert(0, "0")
+
+                while len(expression) > 2:
+                    if not valid_expression(*expression[:3]):
+                        raise InvalidExpressionError
+
+                    operand_left = int(expression.pop(0))
+                    operator = simplify_operator(expression.pop(0))
+                    operand_right = int(expression.pop(0))
+
+                    if operator == "-":
+                        operand_right = -operand_right
+                    expression.insert(0, sum((operand_left, operand_right)))
+
+                print(int(expression.pop()))
+
+        except InvalidExpressionError:
+            print("Invalid expression")
 
 
 def main():
-    stage5()
-    # print(int("a"))
+    stage6()
 
 
 if __name__ == "__main__":
