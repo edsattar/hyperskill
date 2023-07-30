50289b148db57ea42ee5c6d4962d5c1633abba86
edsattar <ahmed.sattar@live.com>
2023-07-30 15:27:28 +0600

    update

--- pyWebScraper/main.py
+++ pyWebScraper/main.py
@@ -1,13 +1,49 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = input("Input the URL:")
+def stage1():
+    url = input("Input the URL:")
 
-r = requests.get(url)
+    r = requests.get(url)
 
-if r:
-    try:
-        print(r.json()['content'])
-    except KeyError:
+    if r:
+        try:
+            print(r.json()['content'])
+        except KeyError:
+            print("Invalid quote resource!")
+    else:
         print("Invalid quote resource!")
-else:
-    print("Invalid quote resource!")
+
+def stage2():
+    # url = input()
+    url = "https://www.nature.com/articles/d41586-023-00103-3"
+
+    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    if r and "articles" in url:
+        title = soup.find('title').text
+        desc = soup.find('meta', {'name': 'description'})['content']
+        if title and desc:
+            # print(f'{{"title": "{title}", "description": "{desc}"}}')
+            # print(str({"title": title, "description": desc}).replace("'", '"'))
+            print({"title": title, "description": desc})
+        else:
+            print('Invalid page!')
+    else:
+        print("Invalid page!")
+
+def stage3():
+    # url = input()
+    url = "http://google.com/asdfg"
+    r = requests.get(url)
+    if r:
+        page_content = r.content
+        with open("source.html", "wb") as f:
+            f.write(page_content)
+        print("Content saved.")
+    else:
+        print(f"The URL returned {r.status_code}!")
+
+if __name__ == "__main__":
+    stage3()
--- pyWebScraper/problems.py
+++ pyWebScraper/problems.py
@@ -2,8 +2,10 @@ import requests
 
 from bs4 import BeautifulSoup
 
-act = input()
+act = int(input())
 url = input()
+# act = 3
+# url = "https://cogniterra.org/media/attachments/lesson/24996/4._Pygmalion.htm" 
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
@@ -11,4 +13,6 @@ soup = BeautifulSoup(r.content, 'html.parser')
 a_tags = soup.find_all('a')
 
 for tag in a_tags:
-    print(tag.text)
+    # print(tag.text.endswith(str(2)))
+    if tag.text.endswith(str(act - 1)):
+        print(tag.get('href'))
